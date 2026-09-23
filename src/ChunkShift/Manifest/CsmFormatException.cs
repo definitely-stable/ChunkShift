03fa6c6307f2f599e31766cb13a7c81ae7c93486
MrFr3di <ynagi-wm@mail.ru>
2026-09-23 18:49:48 +0500

    refactor(csm): remove redundant format exception type

--- src/ChunkShift/Manifest/CsmFormatException.cs
+++
@@ -1,9 +0,0 @@
-namespace ChunkShift.Manifest;
-
-internal sealed class CsmFormatException : InvalidDataException
-{
-    internal CsmFormatException(string message)
-        : base(message)
-    {
-    }
-}
