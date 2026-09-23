76c8d95775349b1e87cefc70765d0903f9f49c41
MrFr3di <ynagi-wm@mail.ru>
2026-09-23 20:25:02 +0500

    feat(cli): add CSM create inspect verify commands

--- src/ChunkShift.Cli/Program.cs
+++ src/ChunkShift.Cli/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+using ChunkShift.Cli;
+
+using var cancellation = new CancellationTokenSource();
+
+Console.CancelKeyPress += (_, eventArgs) =>
+{
+    eventArgs.Cancel = true;
+    cancellation.Cancel();
+};
+
+return await CliApp.RunAsync(
+    args,
+    cancellation.Token);
