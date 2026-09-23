141153aba2ada03e10c1c4a7f783c7f81b3a6159
MrFr3di <ynagi-wm@mail.ru>
2026-09-23 07:33:17 +0500

    fix(bench): make topology benchmark runnable

--- benchmarks/ChunkShift.Benchmarks/CsmTopologyBenchmarks.cs
+++ benchmarks/ChunkShift.Benchmarks/CsmTopologyBenchmarks.cs
@@ -5,7 +5,7 @@ using ChunkShift.Primitives;
 namespace ChunkShift.Benchmarks;
 
 [MemoryDiagnoser]
-public sealed class CsmTopologyBenchmarks : IDisposable
+public class CsmTopologyBenchmarks : IDisposable
 {
     private byte[] _data = null!;
     private MemoryStream _payloadStream = null!;
